92c24a491f5b500da6092bff07bee7f02cb1fe0b
Straw <k7to.so23.ten717@i.softbank.jp>
2022-07-03 21:21:58 +0900

    HTMLテンプレートを呼び出す

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import "github.com/gin-gonic/gin"
 import "net/http"
 
-
 func main() {
-	engine := gin.Default()
-	engine.GET("/", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{
-			"message": "Hello World!",
-		})
-	})
-	engine.Run(":3000")
+    engine := gin.Default()
+    engine.LoadHTMLGlob("static/index.tmpl")
+
+    engine.GET("/", func(context *gin.Context) {
+        context.HTML(http.StatusOK, "index.tmpl", gin.H{
+                    "title": "Hello Gin!",
+                })
+    })
+
+    engine.Run(":3000")
 }
