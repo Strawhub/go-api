31c5e96843c73474fc57ae8f6d976b01c2c704b8
Straw <k7to.so23.ten717@i.softbank.jp>
2022-07-03 22:24:26 +0900

    HTML, CSSを適応

--- main.go
+++ main.go
@@ -5,13 +5,13 @@ import "net/http"
 
 func main() {
     engine := gin.Default()
-    engine.LoadHTMLGlob("static/index.tmpl")
-
-    engine.GET("/", func(context *gin.Context) {
-        context.HTML(http.StatusOK, "index.tmpl", gin.H{
-                    "title": "Hello Gin!",
-                })
-    })
-
-    engine.Run(":3000")
+	// router.Static("URL", "静的ファイル格納場所")
+engine.Static("/static/styles", "./static/styles")
+engine.LoadHTMLGlob("static/index.tmpl")
+engine.GET("/", func(context *gin.Context) {
+	context.HTML(http.StatusOK, "index.tmpl", gin.H{
+	"title": "XXX",
+})
+})
+engine.Run(":3000")
 }
