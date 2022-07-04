83824d947a60ec61fc345c357b23a945707a3f09
Straw <k7to.so23.ten717@i.softbank.jp>
2022-07-04 13:08:00 +0900

    コードの整理

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import "github.com/gin-gonic/gin"
 import "net/http"
 
-func main() {
+func main(){
     engine := gin.Default()
 	// router.Static("URL", "静的ファイル格納場所")
-engine.Static("/static/styles", "./static/styles")
-engine.LoadHTMLGlob("static/index.tmpl")
-engine.GET("/", func(context *gin.Context) {
+	engine.Static("/static", "./static")
+	engine.LoadHTMLGlob("static/index.tmpl")
+	engine.GET("/", func(context *gin.Context) {
 	context.HTML(http.StatusOK, "index.tmpl", gin.H{
 	"title": "XXX",
 })
