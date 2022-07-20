f2056c8ddf392c87fbb70d49bce57d0bd0407b6e
Straw <k7to.so23.ten717@i.softbank.jp>
2022-07-20 11:49:25 +0900

    アクセスログを表示

--- main.go
+++ main.go
@@ -8,20 +8,29 @@ import (
 
 func index(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("static/index.tmpl")
+	if err == nil {
+		log.Println("Start Server")
+	}
 	if err != nil {
 		log.Println(err)
 	}
 	t.Execute(w, "トップページ")
 }
 func database(w http.ResponseWriter, r *http.Request) {
-	t, low := template.ParseFiles("static/db.tmpl")
-	if low != nil {
-		log.Println(low)
+	t, err := template.ParseFiles("static/db.tmpl")
+	if err == nil {
+		log.Println("Access")
+	}
+	if err != nil {
+		log.Println(err)
 	}
 	t.Execute(w, "入力")
 }
 func result(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("static/result.tmpl")
+	if err == nil {
+		log.Println("Access")
+	}
 	if err != nil {
 		log.Println(err)
 	}
